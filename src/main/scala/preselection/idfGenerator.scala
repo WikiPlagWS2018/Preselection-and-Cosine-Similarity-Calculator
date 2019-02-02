a9a769f2c992c7df3cd54d0876aeeaaef7b5814a
Steven <stevenmi@live.de>
2019-02-02 12:47:01 +0100

    Update idfGenerator.scala

--- src/main/scala/preselection/idfGenerator.scala
+++ src/main/scala/preselection/idfGenerator.scala
@@ -4,27 +4,26 @@ import org.apache.spark.{SparkContext, SparkConf}
 import com.datastax.spark.connector._
 import scala.reflect.api.materializeTypeTag
 
-object MainClass {
+object Generator {
     def generate_idf(): Unit = {
 
-      //Create a SparkContext to initialize Spark
+      // Create a SparkContext to initialize Spark
       val conf = new SparkConf()
-//      conf.setMaster("local")
+      // conf.setMaster("local")
       conf.setAppName("[WIKIPLAG] create IDF")
-
 
       conf.set("spark.cassandra.connection.host", "hadoop05.f4.htw-berlin.de")
         .set("spark.cassandra.auth.username", "********") // user name for spark cluster
         .set("spark.cassandra.auth.password", "*********") // password for spark cluster
         //.setMaster("local[*]").setAppName(getClass.getName)
       println("spark config OK")
-
+      
+      // getting RDD
       val sc = new SparkContext(conf)
       val rdd = sc.cassandraTable("wiki2018", "tokens")
 
-      println("start calculation")
-
       //IDF Berechnung
+      println("start calculation")
       val totalDocumentsSize = rdd.count()
       val regex = "^[a-zÀ-ÿ]+$"
       println("Start spark calculation")
